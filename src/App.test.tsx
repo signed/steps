26ec6e85d3c1a55b974596c75276d606a8685cbb
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2021-06-05 22:09:59 +0200

    sample

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,30 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
 
-test('renders learn react link', () => {
+import events from "@testing-library/user-event";
+import React from "react";
+import App from "./App";
+
+// https://github.com/testing-library/user-event/issues/630 initial implementation
+//         https://github.com/testing-library/user-event/releases/tag/v13.1.0
+// https://github.com/testing-library/user-event/issues/646#issuecomment-818289198
+// https://github.com/testing-library/user-event/issues/639#issuecomment-816984104
+// https://github.com/testing-library/user-event/issues/646#issuecomment-818289198 why not skip to the parent
+
+test("vanilla react click", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  return expect(async () =>
+    events.click(await screen.findByText("click me"))
+  ).rejects.toThrow(
+    'unable to click element as it has or inherits pointer-events set to "none".'
+  );
+});
+
+test("chakra click", async () => {
+  render(<App />);
+  await screen.findByRole("button", { name: "Open Menu" });
+  events.click(await screen.findByRole("button", { name: "Open Menu" }));
+  await screen.findByRole("menuitem", {
+    name: /woo\-hoo! t/i,
+  });
+  events.click(await screen.findByRole("menuitem", { name: /Woo-hoo!/ }));
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,67 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import { ChevronDownIcon } from "@chakra-ui/icons";
+import {
+  Button,
+  ChakraProvider,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
+  theme,
+  Text,
+} from "@chakra-ui/react";
+import React, { CSSProperties, MouseEvent } from "react";
 
-const App = () => (
-  <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>
-        Edit <code>src/App.tsx</code> and save to reload.
-      </p>
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
+const toString = (target: string, event: MouseEvent<unknown>) => {
+  const { bubbles, cancelable, button } = event;
+  return JSON.stringify({ target, bubbles, cancelable, button }, null, 2);
+};
+
+const VanillaReact = () => {
+  const spanStyle: CSSProperties = {
+    pointerEvents: "none",
+  };
+  return (
+    <button onClick={(event) => console.log(toString("button", event))}>
+      <span
+        style={spanStyle}
+        onClick={(event) => console.log(toString("span", event))}
       >
-        Learn React
-      </a>
-    </header>
-  </div>
+        click me
+      </span>
+    </button>
+  );
+};
+
+const ChakraFlavour = () => (
+  <ChakraProvider theme={theme}>
+    <Menu>
+      <MenuButton as={Button}>Open Menu</MenuButton>
+      <MenuList onClick={(event) => console.log(toString("MenuList", event))}>
+        <MenuItem
+          onClick={(event) => console.log(toString("MenuItem", event))}
+          command="T"
+        >
+          Woo-hoo!
+        </MenuItem>
+        <MenuItem
+          icon={<ChevronDownIcon />}
+          onClick={(event) => console.log(toString("MenuItem", event))}
+          command="K"
+        >
+          <Text>Crazy</Text>
+          <Text>Split</Text>
+        </MenuItem>
+      </MenuList>
+    </Menu>
+  </ChakraProvider>
 );
+const App = () => {
+  return (
+    <div onClick={(event) => console.log(toString("div", event))}>
+      <ChakraFlavour />
+      <VanillaReact />
+    </div>
+  );
+};
 
 export default App;
